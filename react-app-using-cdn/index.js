4a00fc62dcb1e9a03f2f3aac4a5d4fc661eb4743
SharmaKumarRohit <134692002+SharmaKumarRohit@users.noreply.github.com>
2025-02-08 07:01:02 +0530

    Add files via upload

---
+++ react-app-using-cdn/index.js
@@ -0,0 +1,17 @@
+// const rootElement = document.getElementById('root');
+// const el = React.createElement('h1', null, 'Hello World');
+// ReactDOM.createRoot(rootElement).render(el);
+
+// perform same work using function
+// const rootElement = document.getElementById('root');
+// function HelloWorld() {
+//     return React.createElement("h1", {className: "heading"}, "Hello Rohit");
+// }
+// ReactDOM.createRoot(rootElement).render(HelloWorld());
+
+// perform same work using JSX
+// const rootElement = document.getElementById('root');
+// function HelloWorld() {
+//     return <h1 className="heading">Hello World</h1>;
+// }
+// ReactDOM.createRoot(rootElement).render(<HelloWorld/>);
